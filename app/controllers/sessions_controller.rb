746dd034aec6f6faff529b8b35df724802f526ba
Faisal Hassan <faisal.hassanx@gmail.com>
2017-04-07 22:53:02 +0000

    Authenticate user for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    
+  end
+  
+  def create
+    user = User.find_by(:email params[:session][:email])
+    if user && user.authenticate(params[:session[:password]])
+    render 'new'
+    
+  end
+  
+  def destroy
+    
   end
 end
