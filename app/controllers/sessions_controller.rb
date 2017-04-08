0b168903d3ca62bb9e26a05e7a465942af5a88e0
Faisal Hassan <faisal.hassanx@gmail.com>
2017-04-08 23:20:35 +0000

    Add flash notive for invalid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,13 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(:email params[:session][:email])
-    if user && user.authenticate(params[:session[:password]])
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page
+    else
+      flash[:danger] = "Invalid email/password combination"
     render 'new'
-    
+    end
   end
   
   def destroy
